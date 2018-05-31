ef4d4d72ea5e6813155b7b80c1aab3cba1d21e5f
MaxShang <zshang@me.com>
2018-05-31 11:51:50 -0400

    Update app.R

--- test/app.R
+++ test/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data: wow"),
+   titlePanel("Old Faithful Geyser Data: test2"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
