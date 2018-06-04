43b065d66b6500581c6338ae8f40c3405deac5d0
MaxShang <zshang@me.com>
2018-06-04 11:25:43 -0400

    benchmark2

---
+++ benchmark2/load_packages.R
@@ -0,0 +1,10 @@
+## app.R ##
+library(shinydashboard)
+library(shiny)
+library(googlesheets)
+library(Benchmarking)
+library(ggplot2)
+library(DT)
+library(tidyverse)
+library(shinyBS) # for tooltip
+library(tinytex) # for tooltip
